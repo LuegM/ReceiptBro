f390b0e471bd8512e3b67159ff9fafb4ad59a2f9
Michael Luegmayer <michael.luegmayer@me.com>
2025-10-21 18:06:23 +0200

    Initial Commit

---
+++ ReceiptBro/ReceiptBroApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReceiptBroApp.swift
+//  ReceiptBro
+//
+//  Created by Michael Luegmayer on 21.10.25.
+//
+
+import SwiftUI
+
+@main
+struct ReceiptBroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
